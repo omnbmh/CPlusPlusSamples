77045e5c408a84ffd39ccd210450278ada595517
dezhichen <c8d8z8@gmail.com>
2016-06-13 17:27:03 +0800

    调试下开发环境 可以正常编译

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by c8d8z8@gmail.com on 16/6/13.
+//  Copyright © All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
